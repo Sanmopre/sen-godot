8f079c0ba0eed6140a4f063b380b4470c47622aa
sanmopre <moliner.santiago@gmail.com>
2026-03-21 17:51:19 +0100

    Expendable manager

--- src/managers/expendable_manager.h
+++ src/managers/expendable_manager.h
@@ -1,14 +1,33 @@
-//
-// Created by sanmopre on 3/21/26.
-//
+#pragma once
 
-#ifndef SEN_GODOT_EXPENDABLE_MANAGER_H
-#define SEN_GODOT_EXPENDABLE_MANAGER_H
+#include "physical_entity_manager.h"
 
+// std_fom
+#include "rpr/rpr-physical_v2.0.xml.h"
 
-class expendable_manager
+class ExpendableManager : public PhysicalEntityManager
 {
+    GDCLASS(ExpendableManager, PhysicalEntityManager)
+protected:
+    static void _bind_methods(){}
+
+public:
+    using InterfaceType = rpr::ExpendablesInterface;
+
+public: // RootManager implementation
+    void setInterface(sen::Object* interface, sen::impl::WorkQueue* queue) override
+    {
+        interface_ = dynamic_cast<InterfaceType*>(interface);
+        PhysicalEntityManager::setInterface(interface, queue);
+    }
+
+public: // godot::Node implementation
+    //void _physics_process(double p_delta) override;
+    // void _enter_tree() override;
+    //void _exit_tree() override;
+    // void _ready() override;
+
+private:
+    InterfaceType* interface_ = nullptr;
+    std::string entityType;
 };
-
-
-#endif //SEN_GODOT_EXPENDABLE_MANAGER_H
--- src/utils.h
+++ src/utils.h
@@ -3,7 +3,10 @@
 // std_fom
 #include "rpr/rpr-base_v2.0.xml.h"
 
-[[nodiscard]] inline std::string toString(const rpr::EntityTypeStruct entityType)
+// generated code
+#include "configuration.stl.h"
+
+[[nodiscard]] inline std::string toString(const rpr::EntityTypeStruct& entityType)
 {
     return std::to_string(entityType.category)
     + ":" + std::to_string(entityType.countryCode)
@@ -12,4 +15,35 @@
     + ":" + std::to_string(entityType.extra)
     + ":" + std::to_string(entityType.specific)
     + ":" + std::to_string(entityType.subcategory);
+}
+
+[[nodiscard]] inline configuration::Model getModel(const rpr::EntityTypeStruct& entityType, const configuration::ModelMappingList& mappings)
+{
+    const auto typeString = toString(entityType);
+
+    for (const auto& [disTypes, model] : mappings)
+    {
+        if (const auto it = std::find(disTypes.begin(), disTypes.end(), typeString); it != disTypes.end())
+        {
+            return model;
+        }
+    }
+
+    return configuration::Model::defaultModel;
+}
+
+[[nodiscard]] inline std::string_view getModelPath(const configuration::Model& model)
+{
+    switch (model)
+    {
+    case configuration::Model::defaultModel:
+        return "";
+    case configuration::Model::a400:
+        return "a400";
+    case configuration::Model::f18:
+        return "f18";
+    default:
+        return "";
+    }
+    return "";
 }
